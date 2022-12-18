9ee289035b42c4351100c9b5692510f66aab1db4
AbhiGattineni <abhishekgattineni@gmail.com>
2022-12-17 21:29:29 -0800

    mutual fund data is updated based on latest fetch

---
+++ node/utah/data/mutualfunddatanew.js
@@ -0,0 +1,34 @@
+const mutualfunddata = {
+  meta: {
+    fund_house: "Tata Mutual Fund",
+    scheme_type: "Open Ended Schemes",
+    scheme_category: "Debt Scheme - Medium to Long Duration Fund",
+    scheme_code: 100417,
+    scheme_name:
+      "Tata Income Fund-Regular Plan - Half Yearly Payout of IDCW Option",
+  },
+  data: [
+    {
+      date: "17-12-2022",
+      nav: "15.34140",
+    },
+    {
+      date: "16-12-2022",
+      nav: "15.39990",
+    },
+    {
+      date: "15-12-2022",
+      nav: "15.44080",
+    },
+    {
+      date: "14-12-2022",
+      nav: "15.43150",
+    },
+    {
+      date: "13-12-2022",
+      nav: "15.45280",
+    },
+  ],
+};
+
+module.exports = mutualfunddata;
---
+++ node/utah/readme.md
@@ -0,0 +1,6 @@
+# Hello
+
+1. setup mongobd with moongoose
+2. created get,put,post, delete method in api.js
+3. fetching data from mfapi.in when called in /api/:id
+4. updating nav data from mfapi.in called in /api/update/:id
