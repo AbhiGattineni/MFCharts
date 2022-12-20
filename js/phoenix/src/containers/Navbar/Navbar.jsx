d44b98c2387a92b4d3b84ca636482b559aee25fd
AbhiGattineni <abhishekgattineni@gmail.com>
2022-12-19 21:33:43 -0800

    navigation is fixed

--- js/phoenix/src/containers/Navbar/Navbar.jsx
+++ js/phoenix/src/containers/Navbar/Navbar.jsx
@@ -1,8 +1,19 @@
 import { useAuth } from "../../../src/context/AuthContext";
 import { FaHamburger } from "react-icons/fa";
 import { useState } from "react";
+import Link from "next/link";
+
 import { BareIcon } from "../../components";
 import classNames from "classnames/bind";
+
+const navigationRoutes = [
+  "home",
+  "about",
+  "services",
+  "pricing",
+  "contact",
+  "search",
+];
 
 export const Navbar = () => {
   const { logout } = useAuth();
@@ -18,18 +29,21 @@ export const Navbar = () => {
   };
 
   const [menu, setMenu] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
-        <a href="https://flowbite.com" className="flex items-center">
-          <BareIcon
-            IconComponent={<FaHamburger />}
-            classes={["cursor-pointer"]}
-          />
-          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-            MF Charts
-          </span>
-        </a>
+        <Link href="/">
+          <a href="https://flowbite.com" className="flex items-center">
+            <BareIcon
+              IconComponent={<FaHamburger />}
+              classes={["cursor-pointer"]}
+            />
+            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+              MF Charts
+            </span>
+          </a>
+        </Link>
         <div className="flex items-center md:order-2">
           <button
             type="button"
@@ -109,53 +123,65 @@ export const Navbar = () => {
         >
           <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
             <li>
-              <a
-                href="#"
-                className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
-                aria-current="page"
-              >
-                Home
-              </a>
+              <Link href="/home">
+                <a
+                  href=""
+                  className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+                  aria-current="page"
+                >
+                  Home
+                </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="#"
-                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                About
-              </a>
+              <Link href="/about">
+                <a
+                  href=""
+                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  About
+                </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="#"
-                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Services
-              </a>
+              <Link href="/services">
+                <a
+                  href=""
+                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Services
+                </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="#"
-                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Pricing
-              </a>
+              <Link href="/pricing">
+                <a
+                  href=""
+                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Pricing
+                </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="#"
-                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Contact
-              </a>
+              <Link href="/contact">
+                <a
+                  href=""
+                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Contact
+                </a>
+              </Link>
             </li>
             <li>
-              <a
-                href="#"
-                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Search
-              </a>
+              <Link href="/search">
+                <a
+                  href=""
+                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Search
+                </a>
+              </Link>
             </li>
           </ul>
         </div>
---
+++ js/phoenix/src/pages/dashboard.jsx
@@ -0,0 +1,75 @@
+import Link from "next/link";
+
+import { Card, Table, Label } from "../components";
+import { Navbar } from "../containers";
+
+const Dashboard = () => {
+  return (
+    <div className="flex flex-col">
+      <Navbar />
+      <div className="flex justify-center items-center basis-1/3">
+        <div className="basis-1/3 p-10">
+          <Card>
+            <div className="flex">
+              <div className="m-4">
+                <Label text="Name" classes={["text-3xl , text-white"]} />
+                <Label
+                  text="Abhishek Gattineni"
+                  classes={["text-xs , text-white"]}
+                />
+              </div>
+              <div className="m-4">
+                <Label text="Email" classes={["text-3xl , text-white"]} />
+                <Label
+                  text="abhishekgattineni@gmail.com"
+                  classes={["text-xs , text-white"]}
+                />
+              </div>
+            </div>
+          </Card>
+        </div>
+        <div className="basis-2/3 p-10">
+          <Card>
+            <div className="flex">
+              <div className="m-4">
+                <Label
+                  text="Investment Value"
+                  classes={["text-xs , text-white"]}
+                />
+                <Label text="$23,000" classes={["text-3xl , text-white"]} />
+              </div>
+              <div className="m-4">
+                <Label
+                  text="Current Value"
+                  classes={["text-xs , text-white"]}
+                />
+                <Label text="$93,000" classes={["text-3xl , text-white"]} />
+              </div>
+            </div>
+            <div className="flex">
+              <div className="m-4">
+                <Label
+                  text="Total Gain/Loss"
+                  classes={["text-xs , text-white"]}
+                />
+                <Label text="$23,000" classes={["text-3xl , text-white"]} />
+              </div>
+              <div className="m-4">
+                <Label
+                  text="Today's Gain/Loss"
+                  classes={["text-xs , text-white"]}
+                />
+                <Label text="$23,000" classes={["text-3xl , text-white"]} />
+              </div>
+            </div>
+          </Card>
+        </div>
+      </div>
+      <div className="flex p-10 flex-col">
+        <Table />
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
