38c81059a1f3983296e5c60f8f0c9c6b15143eae
AbhiGattineni <abhishekgattineni@gmail.com>
2023-01-08 02:02:52 -0800

    firebase env not set yet

--- js/phoenix/src/config/firebase.js
+++ js/phoenix/src/config/firebase.js
@@ -4,15 +4,11 @@ import {
   GoogleAuthProvider,
   FacebookAuthProvider,
 } from "firebase/auth";
-
 const firebaseConfig = {
   apiKey: "AIzaSyB_xmapMV36zOYh3bfnNU6W82wRc6aoOMM",
   authDomain: "mfchartsv2.firebaseapp.com",
   projectId: "mfchartsv2",
 };
-// apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
-// authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-// projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
 
 const app = initializeApp(firebaseConfig);
 
--- js/phoenix/src/pages/index.js
+++ js/phoenix/src/pages/index.js
@@ -3,9 +3,10 @@ import Link from "next/link";
 import { Card, Table, Label } from "../components";
 import { Navbar } from "../containers";
 import Layout from "./Layout";
+import Home from "./home";
 
 const Index = () => {
-  return <h1>Index page</h1>;
+  return <Home />;
 };
 
 export default Index;
