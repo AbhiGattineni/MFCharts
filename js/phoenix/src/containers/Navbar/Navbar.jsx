39153c93522153870133add6f8dff68c50d79f70
AbhiGattineni <abhishekgattineni@gmail.com>
2023-01-08 13:53:48 -0800

    navbar changed to home,portfolio, contact, search

--- js/phoenix/src/containers/Navbar/Navbar.jsx
+++ js/phoenix/src/containers/Navbar/Navbar.jsx
@@ -134,35 +134,16 @@ export const Navbar = () => {
               </Link>
             </li>
             <li>
-              <Link href="/about">
+              <Link href="/portfolio">
                 <a
                   href=""
                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
-                  About
+                  Portfolio
                 </a>
               </Link>
             </li>
-            <li>
-              <Link href="/services">
-                <a
-                  href=""
-                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Services
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/pricing">
-                <a
-                  href=""
-                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Pricing
-                </a>
-              </Link>
-            </li>
+
             <li>
               <Link href="/contact">
                 <a
