e8c018d2b846313a10556fcb072a4e5c4a6975b2
AbhiGattineni <abhishekgattineni@gmail.com>
2023-01-12 22:01:40 -0800

    accordian changed sightly

--- js/phoenix/src/components/Accordion/Accordion.jsx
+++ js/phoenix/src/components/Accordion/Accordion.jsx
@@ -1,14 +1,50 @@
 import { Collapse } from "react-collapse";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+import { Button } from "../Button/Button";
 
-export const Accordion = ({ open, toggle, title, desc }) => {
+export const Accordion = ({
+  open,
+  toggle,
+  fund,
+  desc,
+  qty,
+  avg,
+  holding,
+  market,
+  today,
+}) => {
   return (
     <div className="pt-[10px] border-2 border-gray-300 rounded-sm mt-3">
       <div
         className="bg-white py-[25px] px-[50px] flex justify-between items-center cursor-pointer"
         onClick={toggle}
       >
-        <p className="text-[22px] font-semibold">{title}</p>
+        <div className="text-[22px] font-semibold grid grid-cols-6 w-full">
+          <div className="grid grid-row-6">
+            <text className="text-xs">Fund:</text>
+            <text className="text-xs sm:text-xl">{fund}</text>
+          </div>
+          <div className="grid grid-row-6">
+            <text className="text-xs">Qty:</text>
+            <text className="text-xs sm:text-xl">{qty}</text>
+          </div>
+          <div className="grid grid-row-6">
+            <text className="text-xs">Avg:</text>
+            <text className="text-xs sm:text-xl">{avg}</text>
+          </div>
+          <div className="grid grid-row-6">
+            <text className="text-xs">Holding Val:</text>
+            <text className="text-xs sm:text-xl">{holding}</text>
+          </div>
+          <div className="grid grid-row-6">
+            <text className="text-xs">Market Value:</text>
+            <text className="text-xs sm:text-xl">{market}</text>
+          </div>
+          <div className="grid grid-row-6">
+            <text className="text-xs">Today P/L:</text>
+            <text className="text-xs sm:text-xl">{today}</text>
+          </div>
+        </div>
         <div className="text-[30px]">
           {open ? <AiOutlineMinus /> : <AiOutlinePlus />}
         </div>
--- js/phoenix/src/pages/portfolio.jsx
+++ js/phoenix/src/pages/portfolio.jsx
@@ -1,12 +1,36 @@
 import { useState } from "react";
-import { Accordion } from "../components";
+import { Accordion, Button } from "../components";
 
 const Portfolio = () => {
   const [open, setOpen] = useState(false);
   const accordionData = [
-    { title: "one", desc: "none" },
-    { title: "two", desc: "node" },
-    { title: "three", desc: "three" },
+    {
+      title: "Tata",
+      qty: "20",
+      avg_price: "2.64",
+      holding_val: "1776.32",
+      market_val: "3028.85",
+      today_pl: "2994",
+      desc: "none",
+    },
+    {
+      title: "Reliance",
+      qty: "20",
+      avg_price: "2.64",
+      holding_val: "1776.32",
+      market_val: "3028.85",
+      today_pl: "2994",
+      desc: "node",
+    },
+    {
+      title: "Vedantha",
+      qty: "20",
+      avg_price: "2.64",
+      holding_val: "1776.32",
+      market_val: "3028.85",
+      today_pl: "2994",
+      desc: "three",
+    },
   ];
   const toggle = (index) => {
     if (open === index) {
@@ -15,18 +39,31 @@ const Portfolio = () => {
     setOpen(index);
   };
   return (
-    <div className="container mx-auto">
-      {accordionData.map((data, index) => {
-        return (
-          <Accordion
-            key={index}
-            open={index === open}
-            title={data.title}
-            toggle={() => toggle(index)}
-            desc={data.desc}
-          />
-        );
-      })}
+    <div className="container mx-auto ">
+      <div
+        className="grid justify-items-center sm:justify-items-end"
+        classes={["rounded"]}
+      >
+        <Button text={"Add Funds to Portfolio"} />
+      </div>
+      <div>
+        {accordionData.map((data, index) => {
+          return (
+            <Accordion
+              key={index}
+              open={index === open}
+              fund={data.title}
+              toggle={() => toggle(index)}
+              desc={data.desc}
+              qty={data.qty}
+              avg={data.avg_price}
+              holding={data.holding_val}
+              market={data.market_val}
+              today={data.today_pl}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
