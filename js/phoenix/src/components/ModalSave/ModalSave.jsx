f5a273d45563280bbc850d6fde83255f2155eb02
AbhiGattineni <abhishekgattineni@gmail.com>
2023-01-19 11:58:18 -0800

    mosal for saving portfolio is created

---
+++ js/phoenix/src/components/ModalSave/ModalSave.jsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from "react";
+
+import { Button, Datepicker, Input } from "../../components";
+import { AddPortfolioFunds } from "../../containers";
+
+export function ModalSave() {
+  const [showModal, setShowModal] = React.useState(false);
+  const [saveLabel, setSaveLabel] = useState("");
+
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
+  return (
+    <>
+      <Button
+        type="button"
+        classes={["w-1/2", "blue", "text-black"]}
+        handleClick={() => setShowModal(true)}
+        text="Save"
+      />
+      {showModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none mx-3 sm:mx-0">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">
+                    Enter Portfolio Name To Save
+                  </h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <Input
+                    placeholder={"Enter label you want to save"}
+                    classes={["rounded"]}
+                    setValue={setSaveLabel}
+                  />
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={handleClose}
+                  >
+                    Close
+                  </button>
+                  <button
+                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={handleClose}
+                  >
+                    Save
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
+  );
+}
--- js/phoenix/src/pages/search.jsx
+++ js/phoenix/src/pages/search.jsx
@@ -1,12 +1,11 @@
 import { useEffect, useState } from "react";
 import { BsSave } from "react-icons/bs";
 
-import { Button, FetchAllMf, Input, BareIcon } from "../components";
+import { Button, FetchAllMf, Input, BareIcon, ModalSave } from "../components";
 import { LineGraph } from "../containers";
 
 const Search = () => {
   const [navData, setNavData] = useState({});
-  const [saveLabel, setSaveLabel] = useState("");
 
   const handleClick = () => {};
 
@@ -30,23 +29,7 @@ const Search = () => {
     <div>
       <FetchAllMf setNavData={(e) => handleNavData(e)} isMulti={true} />
       <div className="mt-1 md:mt-5">
-        {navData != null ? (
-          <div className="grid grid-cols-6">
-            <Input
-              placeholder={"Enter label you want to save"}
-              classes={["rounded"]}
-              setValue={setSaveLabel}
-            />
-            <div onClick={handleClick}>
-              <BareIcon
-                IconComponent={<BsSave />}
-                classes={["cursor-pointer"]}
-              />
-            </div>
-          </div>
-        ) : (
-          ""
-        )}
+        {navData != null ? <ModalSave /> : ""}
 
         <div className="grid grid-cols-1 md:grid-cols-2">
           {/* <div>
