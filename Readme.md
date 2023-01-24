43e5f988a1439d9899554d66a2bd460a3a9ae6ed
AbhiGattineni <abhishekgattineni@gmail.com>
2023-01-23 23:11:20 -0900

    user story created

--- Readme.md
+++ Readme.md
@@ -1,15 +1,23 @@
 # MFCharts V2
+
 Portfolio management tool
+
+## Documentation
+
+- Here is the documentation for User stories, data modelling, API's
+  [Google Doc](https://docs.google.com/document/d/1v8wk0hIdS6WpyUDx7LQsPvHGOFgC3on_GuQvgGMuE3s/edit?usp=sharing)
 
 ## Installation
 
 ## Main branch restictions
+
 - To push changes in main branch, follow below steps
   - Create a new branch from main and work on your changes
   - Push the new branch to github
   - Create a new pullrequest w.r.t main branch in github UI
 
 ## Project Structure
+
 - root
   - js
     - phoenix
@@ -30,8 +38,10 @@ Portfolio management tool
     - new service
       - ...
 
-****
+---
+
 ## Commit Structure
+
 ```bash
 <language>(<project name>): <One liner commit message>
 
@@ -40,6 +50,7 @@ Portfolio management tool
 
 <Jira ticket number>
 ```
-****
+
+---
 
 ## Credits
--- js/phoenix/src/pages/signup.jsx
+++ js/phoenix/src/pages/signup.jsx
@@ -1,38 +1,36 @@
 import { SignupForm } from "../containers";
-import { BareIcon } from "../../src/components"
+import { BareIcon, Separator } from "../../src/components";
 import { BsLinkedin, BsGoogle, BsGithub } from "react-icons/bs";
-import Separator from "../components/Separator/Separator";
 
 const Signup = () => {
-    return (
+  return (
+    <div className="flex flex-row">
+      <div className="basis-1 md:basis-2/3  flex flex-col justify-center items-center">
+        <h1 className="font-bold text-3xl">Add Details to Signup</h1>
+        <SignupForm />
+        <Separator />
+        <div className="font-bold text-xs">Signup with</div>
         <div className="flex flex-row">
-            <div className="basis-1 md:basis-2/3  flex flex-col justify-center items-center">
-                <h1 className="font-bold text-3xl">Add Details to Signup</h1>
-                <SignupForm />
-                <Separator />
-                <div className="font-bold text-xs">Signup with</div>
-                <div className="flex flex-row">
-                    <BareIcon IconComponent={<BsGoogle />} classes={["text-red-500"]} />
-                    <BareIcon IconComponent={<BsGithub />} />
-                    <BareIcon IconComponent={<BsLinkedin />} />
-                </div>
-            </div>
-            <div className="md:basis-1/3 md:bg-bgColor hidden md:flex md:flex-col md:justify-center md:items-center">
-                <div className="font-bold text-2xl">Already A Member?</div>
-                <div className="mt-6 font-bold">
-                    <div>
-                        Sign in and discover a great
-                    </div>
-                    <div>
-                        amount of new opportunities!
-                    </div>
-                </div>
-                <button className="mt-6 shadow bg-white hover:bg-gray-100 focus:shadow-outline focus:outline-none text-black font-bold py-2 px-4 rounded-full mb-6" type="button">
-                    Sign In
-                </button>
-            </div>
+          <BareIcon IconComponent={<BsGoogle />} classes={["text-red-500"]} />
+          <BareIcon IconComponent={<BsGithub />} />
+          <BareIcon IconComponent={<BsLinkedin />} />
         </div>
-    );
-}
+      </div>
+      <div className="md:basis-1/3 md:bg-bgColor hidden md:flex md:flex-col md:justify-center md:items-center">
+        <div className="font-bold text-2xl">Already A Member?</div>
+        <div className="mt-6 font-bold">
+          <div>Sign in and discover a great</div>
+          <div>amount of new opportunities!</div>
+        </div>
+        <button
+          className="mt-6 shadow bg-white hover:bg-gray-100 focus:shadow-outline focus:outline-none text-black font-bold py-2 px-4 rounded-full mb-6"
+          type="button"
+        >
+          Sign In
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Signup;
