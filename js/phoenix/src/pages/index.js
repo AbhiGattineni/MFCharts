d71a2509a015deb1be0bb1f8baf44982f057aa4b
StephenJakku <uttamjakku96@gmail.com>
2023-01-27 10:46:36 -0800

    removed Dashboard page and routed it to Home page

--- js/phoenix/src/pages/index.js
+++ js/phoenix/src/pages/index.js
@@ -1,5 +1,29 @@
-const Index = () => {
-  return;
+import { useRouter } from "next/router";
+
+import {
+  DashboardDashboard,
+  DashboardPortfolio,
+  DashboardSearch,
+  DashboardWatchlist,
+} from "../containers";
+
+const Home = () => {
+  return (
+    <div className="container mx-auto grid justify-items-center grid-rows-4">
+      <div className="grid row-span-1 w-full">
+        <DashboardDashboard />
+      </div>
+      <div className="grid row-span-1 w-full">
+        <DashboardSearch />
+      </div>
+      <div className="grid row-span-1 w-full">
+        <DashboardPortfolio />
+      </div>
+      <div className="grid row-span-1 w-full">
+        <DashboardWatchlist />
+      </div>
+    </div>
+  );
 };
 
-export default Index;
+export default Home;
