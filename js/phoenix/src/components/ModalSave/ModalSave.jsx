c91a238ca495fbd8e2ddd4a68c3156be9e5533d7
AbhiGattineni <abhishekgattineni@gmail.com>
2023-02-07 14:29:08 -0800

    handle save and handle close are separeted in the modalsave component

--- js/phoenix/src/components/ModalSave/ModalSave.jsx
+++ js/phoenix/src/components/ModalSave/ModalSave.jsx
@@ -7,9 +7,13 @@ export function ModalSave({ saveData }) {
   const [showModal, setShowModal] = React.useState(false);
   const [saveLabel, setSaveLabel] = useState("");
 
-  const handleClose = () => {
+  const handleSave = () => {
     setShowModal(false);
     saveData(saveLabel);
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
   };
 
   return (
@@ -64,7 +68,7 @@ export function ModalSave({ saveData }) {
                   <button
                     className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
-                    onClick={handleClose}
+                    onClick={handleSave}
                   >
                     Save
                   </button>
