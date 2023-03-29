2f2711e489422df36cc03248b68aeaec4f6f779b
abhigattineni <abhishekgattineni@gmail.com>
2023-03-29 06:17:26 -0700

    nodejs-error03/29/2023

--- js/phoenix/src/containers/DashboardPortfolio/DashboardPortfolio.jsx
+++ js/phoenix/src/containers/DashboardPortfolio/DashboardPortfolio.jsx
@@ -5,6 +5,7 @@ import { Button } from "../../components";
 export const DashboardPortfolio = ({ setDashboardPortfolio }) => {
   const [portfolioNames, setPortfolioNames] = useState({});
   useEffect(() => {
+    console.log("dashboard id", auth.currentUser.uid);
     fetch(`http://127.0.0.1:5000/api/userPortfolio/${auth.currentUser.uid}`)
       .then((response) => response.json())
       .then((data) => {
@@ -18,15 +19,17 @@ export const DashboardPortfolio = ({ setDashboardPortfolio }) => {
         Portfolio Funds
       </div>
       <div className="grid grid-cols-1 p-3">
-        {Object.keys(portfolioNames).map((e, index) => {
-          return (
-            <Button
-              text={portfolioNames[e].schemeName}
-              classes={["rounded"]}
-              handleClick={() => setDashboardPortfolio(portfolioNames[e])}
-            />
-          );
-        })}
+        {Object.keys(portfolioNames).length != 0
+          ? Object.keys(portfolioNames).map((e, index) => {
+              return (
+                <Button
+                  text={portfolioNames[e].schemeName}
+                  classes={["rounded"]}
+                  handleClick={() => setDashboardPortfolio(portfolioNames[e])}
+                />
+              );
+            })
+          : "There are no portfolio Names"}
       </div>
     </div>
   );
