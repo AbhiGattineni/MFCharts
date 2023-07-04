ecf891fb44cb602189da680a7a593bb67867b13c
abhigattineni <abhishekgattineni@gmail.com>
2023-07-04 17:17:37 -0400

    chnages in portfolio dropdown

--- js/phoenix/src/containers/PortfolioDropdown/PortfolioDropdown.jsx
+++ js/phoenix/src/containers/PortfolioDropdown/PortfolioDropdown.jsx
@@ -16,14 +16,14 @@ const PortfolioDropdown = ({ data }) => {
       </div>
       <div className="col-span-12 grid grid-cols-12 gap-4 p-4 items-center">
         <div className="col-span-12 sm:col-span-9 md:col-span-12 lg:col-span-12">
-          <div className="flex flex-col sm:flex-row sm:items-center ">
+          <div className="flex flex-col sm:flex-row sm:items-center">
             <textarea
-              className="flex-grow border border-gray-300 rounded-md py-2 px-3 resize-y sm:mr-2 sm:mb-0 mb-2"
+              className="flex-grow border border-gray-300 rounded-md py-2 px-3 resize-y sm:mr-2 sm:mb-0 mb-2 h-[2.5rem]"
               placeholder="Add Timeline"
               value={inputValue}
               onChange={handleChange}
             />
-            <button className="flex-grow-0 flex-shrink-0 sm:w-1/4 bg-blue-500 text-white rounded-md py-2">
+            <button className="flex-grow-0 flex-shrink-0 sm:w-1/4 bg-blue-500 text-white rounded-md py-2 h-[2.5rem]">
               Add Timeline
             </button>
           </div>
