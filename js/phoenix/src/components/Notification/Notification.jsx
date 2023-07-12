ce3feba7a1cc49b49566aa74747d7dfaa78b32ce
Trinath Gundla <84904193+GundlaTrinath@users.noreply.github.com>
2023-07-12 17:44:57 +0530

    Search page updated with existing watchlist and functionalities

---
+++ js/phoenix/src/components/Notification/Notification.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Notification = ({ message, type }) => {
+  const toastClasses = {
+    success: 'bg-green-500',
+    error: 'bg-red-500',
+    info: 'bg-blue-500',
+  };
+
+  return (
+    <div className={`fixed bottom-0 left-0 m-4 p-4 rounded-md text-white font-semibold ${toastClasses[type]}`}>
+      {message}
+    </div>
+  );
+};
+
+export default Notification;
